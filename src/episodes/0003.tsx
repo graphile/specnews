7e37d8604755e3a0695c3299d9257c21d1842a7e
Benjie Gillam <benjie@jemjie.com>
2022-06-07 18:49:25 +0100

    Upload episode 3

--- src/episodes/0003.tsx
+++ src/episodes/0003.tsx
@@ -4,20 +4,25 @@ import type { EpisodeDetails } from "./interfaces";
 export const ep0003: EpisodeDetails = {
   supertitle: "SpecNews #3",
   title: "June 2022 WG",
-  embed: "https://anchor.fm/specnews/embed/episodes/0--Hello-World-e1jghe1",
+  embed: "https://anchor.fm/specnews/embed/episodes/3--June-2022-WG-e1jkq0r",
   date: "2022-06-07",
   description: (
     <div>
       <p>
-        The actual launch date of this podcast. The actual launch date of this
-        podcast. The actual launch date of this podcast. The actual launch date
-        of this podcast. The actual launch date of this podcast. The actual
-        launch date of this podcast.
+        June 2022's working-group-meeting saw the closure of a number of action
+        items, the introduction of a new "composite schemas" working group, a
+        discussion around an “experimental” directive, a new validation rule to
+        assert operation types exist, an update on the client-controlled
+        nullability, oneOf, and "unions implementing interfaces" RFCs and a
+        number of spicy takes on the GraphQL spec itself!
       </p>
-
       <p>
         Music by{" "}
-        <a href="https://pixabay.com/users/zen_man-4257870/">
+        <a
+          href="https://pixabay.com/users/zen_man-4257870/"
+          rel="ugc noopener noreferrer"
+          target="_blank"
+        >
           Zen_Man on Pixabay
         </a>
         .
