e2de034f2d411714fddac5b0d5224a97cac0443f
Benjie Gillam <benjie@jemjie.com>
2022-07-13 14:12:36 +0100

    Add episode 4

---
+++ src/episodes/0004.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import type { EpisodeDetails } from "./interfaces";
+
+export const ep0004: EpisodeDetails = {
+  supertitle: "SpecNews #4",
+  title: "July 2022 WG",
+  embed: "https://anchor.fm/specnews/embed/episodes/4--July-2022-WG-e1l5vdr",
+  date: "2022-07-07",
+  description: (
+    <div>
+      <p>
+        July 2022&apos;s working-group-meeting saw discussion around adding
+        directives to existing enum values, adding "schema metadata" to SDL and
+        introspection, allowing recursion when resolving the concrete object
+        type of a value representing an interface or union type, an update from
+        the GraphQL-over-HTTP working group, and an update on the defer and
+        stream RFC.
+      </p>
+      <p>
+        Benjie&apos;s GraphQL Conference talk on Schema Metadata:{" "}
+        <a
+          href="https://www.youtube.com/watch?v=c1oa7p73rTw"
+          rel="ugc noopener noreferrer"
+          target="_blank"
+        >
+          youtube.com/watch?v=c1oa7p73rTw
+        </a>
+      </p>
+      <p>
+        Music by{" "}
+        <a
+          href="https://pixabay.com/users/zen_man-4257870/"
+          rel="ugc noopener noreferrer"
+          target="_blank"
+        >
+          Zen_Man on Pixabay
+        </a>
+        .
+      </p>
+    </div>
+  ),
+};
--- src/episodes/index.tsx
+++ src/episodes/index.tsx
@@ -2,6 +2,7 @@ import { ep0000 } from "./0000";
 import { ep0001 } from "./0001";
 import { ep0002 } from "./0002";
 import { ep0003 } from "./0003";
+import { ep0004 } from "./0004";
 
 export { EpisodeDetails } from "./interfaces";
-export default [ep0003, ep0002, ep0001, ep0000];
+export default [ep0004, ep0003, ep0002, ep0001, ep0000];
