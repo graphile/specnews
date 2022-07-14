18006d4360c1448624124b8c375e48301a69ca2b
Benjie Gillam <benjie@jemjie.com>
2022-07-14 15:44:47 +0100

    Add missing meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,12 @@ import episodes from "../episodes/index";
 import { Episode } from "../components/Episode";
 import "../styles/index.css";
 import { Hero } from "../components/Hero";
+import { Meta } from "../components/Meta";
 
 const IndexPage = () => {
   return (
     <div>
+      <Meta />
       <Hero />
       <div className="main">
         <main>
