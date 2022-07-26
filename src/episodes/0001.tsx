7277a933cdb4806e49f7cedfd4afd696e50052ff
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2022-07-26 14:22:51 +0100

    Episode 1 page

--- src/episodes/0001.tsx
+++ src/episodes/0001.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { EpisodeDetails } from "./interfaces";
+import { Transcript, J, B, Timestamp } from "../components/Transcript";
 
 export const ep0001: EpisodeDetails = {
   id: "ep0001",
@@ -28,5 +29,53 @@ export const ep0001: EpisodeDetails = {
         .
       </p>
     </div>
+  ),
+  transcript: (
+    <Transcript>
+      <B>
+        Welcome to episode 1 of Spec News</B>
+      <J>
+        keeping you up to date with advances in the GraphQL Spec and related foundation projects.
+      </J>
+      <B>
+        We're your bespectacled hosts: I'm Benjie
+      </B>
+      <J>
+        and I'm Jem.
+        April’s working group saw the announcement of the GraphQL Foundation’s first Conference
+      </J>
+      <B>a preview of the future direction of GraphiQL</B>
+      <J>
+      a proposal to allow unions to implement interfaces
+      </J>
+      <B>
+      and a discussion of the payload format to use for streams
+      </B>
+      <J>
+        The first item was rather exciting - Lee introduced the first official conference organised by the GraphQL Foundation. To make things easier, the conference will be colocated with OpenJSWorld on June 7th and 8th in Austin, but it's important to note that the GraphQL part is not JavaScript-specific!
+      </J>
+      <B>
+        Rather than being an end-user conference, the audience is expected to consist of working group attendees, GraphQL implementers, and other people well versed in topics around the GraphQL specification. Very exciting - get your talk submissions in now!
+      </B>
+      <Timestamp>[Time Stamp] Title</Timestamp>
+      <J>
+
+      </J>
+      <B>
+        
+      </B>
+      <J>
+
+      </J>
+      <B>
+
+      </B>
+      <J>
+
+      </J>
+      <B>
+
+      </B>
+    </Transcript>
   ),
 };
