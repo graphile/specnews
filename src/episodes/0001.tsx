d3029bb8a8bc1d2146a3620bd412c899c68c137f
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2022-07-26 18:05:36 +0100

    em dash all the things for Benjie

--- src/episodes/0001.tsx
+++ src/episodes/0001.tsx
@@ -11,8 +11,8 @@ export const ep0001: EpisodeDetails = {
   description: (
     <div>
       <p>
-        April 2022’s working group saw the announcement of the GraphQL
-        Foundation’s first conference, a preview of the future direction of
+        April 2022&apos;s working group saw the announcement of the GraphQL
+        Foundation&apos;s first conference, a preview of the future direction of
         GraphiQL, a proposal to allow unions to implement interfaces, and a
         discussion of the payload format to use for streams. Jem and Benjie give
         you the lowdown.
@@ -37,12 +37,12 @@ export const ep0001: EpisodeDetails = {
         keeping you up to date with advances in the GraphQL Spec and related
         foundation projects.
       </J>
-      <B>We're your bespectacled hosts: I'm Benjie</B>
+      <B>We&apos;re your bespectacled hosts: I&apos;m Benjie</B>
       <J>
-        and I'm Jem.
+        and I&apos;m Jem.
         <p>
-          April’s working group saw the announcement of the GraphQL Foundation’s
-          first Conference
+          April&apos;s working group saw the announcement of the GraphQL
+          Foundation&apos;s first Conference
         </p>
       </J>
       <B>a preview of the future direction of GraphiQL</B>
@@ -50,44 +50,43 @@ export const ep0001: EpisodeDetails = {
       <B>and a discussion of the payload format to use for streams</B>
       <Timestamp>[0:33] GraphQL Foundation Conference</Timestamp>
       <J>
-        The first item was rather exciting - Lee introduced the{" "}
+        The first item was rather exciting&mdash;Lee introduced the{" "}
         <a href="https://graphql.org/foundation/graphql-conf/" target="_new">
           first official conference organised by the GraphQL Foundation
         </a>
         . To make things easier, the conference will be colocated with
-        OpenJSWorld on June 7th and 8th in Austin, but it's important to note
-        that the GraphQL part is not JavaScript-specific!
+        OpenJSWorld on June 7th and 8th in Austin, but it&apos;s important to
+        note that the GraphQL part is not JavaScript&#8208;specific!
       </J>
       <B>
-        Rather than being an end-user conference, the audience is expected to
-        consist of working group attendees, GraphQL implementers, and other
+        Rather than being an end&#8208;user conference, the audience is expected
+        to consist of working group attendees, GraphQL implementers, and other
         people well versed in topics around the GraphQL specification. Very
-        exciting - get your talk submissions in now!
+        exciting&mdash;get your talk submissions in now!
       </B>
       <Timestamp>[1:11] Update on GraphiQL</Timestamp>
       <J>Tim joined us to give us an update on the GraphiQL&mdash;</J>
-      <B>that's Graph i Q L – the GraphQL IDE</B>
+      <B>that&apos;s &quot;Graph i Q L&quot;: the GraphQL IDE</B>
       <J>
-        &mdash;version 2 design proposals. What was presented looks fantastic -
-        a minimalist and flexible design that expands as you become more
-        familiar. The reception was very positive, tempered only by the working
-        group attendees suggesting improvements to the design, for example
-        allowing additional space for longer-form markdown documentation,
-        improving discoverability of variables, and other such matters. I hope
-        Tim found the feedback helpful and we can't wait to see the project
-        advance!
+        &mdash;version 2 design proposals. What was presented looks fantastic: a
+        minimalist and flexible design that expands as you become more familiar.
+        The reception was very positive, tempered only by the working group
+        attendees suggesting improvements to the design, for example allowing
+        additional space for longer&#8208;form markdown documentation, improving
+        discoverability of variables, and other such matters. I hope Tim found
+        the feedback helpful and we can&apos;t wait to see the project advance!
       </J>
       <Timestamp>[1:54] Unions declaring interfaces</Timestamp>
       <B>
         Next up was Yaacov with a proposal to allow unions to declare
         implementation of interfaces. After some discussion this boiled down to
-        a constraint - the union doesn't itself implement the interfaces,
-        instead it requires that all object types within it implement the
-        interfaces - an interesting proposal. The working group pushed Yaacov to
-        consider allowing fields in the interface to be resolved directly on the
-        selection set (without the need for a fragment) for consistency with
-        interfaces elsewhere in the spec. The balance as always is between
-        simplicity and power.
+        a constraint&mdash;the union doesn&apos;t itself implement the
+        interfaces, instead it requires that all object types within it
+        implement the interfaces&mdash;an interesting proposal. The working
+        group pushed Yaacov to consider allowing fields in the interface to be
+        resolved directly on the selection set (without the need for a fragment)
+        for consistency with interfaces elsewhere in the spec. The balance as
+        always is between simplicity and power.
       </B>
       <J>
         To advance the proposal further, Lee suggested that Yaacov:
@@ -97,11 +96,11 @@ export const ep0001: EpisodeDetails = {
           </li>
           <li>looks for and documents precedent from other languages;</li>
           <li>
-            and considers if a different keyword rather than "implements" would
-            make sense.
+            and considers if a different keyword rather than
+            &quot;implements&quot; would make sense.
           </li>
         </ul>
-        The proposal was advanced to RFC stage 1 - &quot;Proposal&quot;.
+        The proposal was advanced to RFC stage 1: &quot;Proposal&quot;.
       </J>
       <B>And may well make it harder for me to deprecate unions!</B>
       <Timestamp>[3:01] The payload format to use for streams</Timestamp>
@@ -110,7 +109,7 @@ export const ep0001: EpisodeDetails = {
         the payload format to use for streams. It was established that nulls
         must bubble only to the boundary of the payload and no further
       </J>
-      <B>(since you can't go back in time)</B>
+      <B>(since you can&apos;t go back in time)</B>
       <J>
         indeed; however the challenge is in distinguishing between actual nulls
         and errors.
@@ -130,23 +129,24 @@ export const ep0001: EpisodeDetails = {
           included in the payload, and if so: where? One consideration here is
           potential future batching which was discussed in some depth, along
           with possible payload shapes for including multiple stream or defer
-          payloads in the same GraphQL response payload - for example using an
-          "incremental" field - this should help to avoid client "thrashing".
+          payloads in the same GraphQL response payload&mdash;for example using
+          an &quot;incremental&quot; field&mdash;this should help to avoid
+          client &quot;thrashing&quot;.
         </p>
       </J>
       <B>
         <p>
-          Definitely more investigation to be done here, but there’s at least
-          consensus to put the start index in the path.
+          Definitely more investigation to be done here, but there&apos;s at
+          least consensus to put the start index in the path.
         </p>
         <p>
-          Yaacov then guided the conversation to the topic of "async iterators
-          of iterables," a rather JavaScript-specific topic but one which may
-          have influence on other implementations.
+          Yaacov then guided the conversation to the topic of &quot;async
+          iterators of iterables&quot;, a rather JavaScript&#8208;specific topic
+          but one which may have influence on other implementations.
         </p>
       </B>
       <J>
-        It's common for other implementations to be translations of the
+        It&apos;s common for other implementations to be translations of the
         reference implementation into other programming languages.
       </J>
       <B>
@@ -158,8 +158,9 @@ export const ep0001: EpisodeDetails = {
       <J>
         The question also extended to returning the data in this shape via the
         GraphQL response, however it was pointed out that after compression the
-        difference between the solutions would be marginal so this shouldn't be
-        a weighty consideration. Further discussion was redirected to{" "}
+        difference between the solutions would be marginal so this
+        shouldn&apos;t be a weighty consideration. Further discussion was
+        redirected to{" "}
         <a
           href="https://github.com/graphql/graphql-wg/discussions/939"
           target="_new"
