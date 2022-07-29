5f217b0ed87c81b6a699db5cdbff71b98b2fc771
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2022-07-29 10:12:01 +0100

    inline style for introduction

--- src/components/Transcript.tsx
+++ src/components/Transcript.tsx
@@ -1,6 +1,10 @@
 import React, { FC, PropsWithChildren } from "react";
 import { SignupFormLight } from "./SignupForm";
 
+interface X {
+  x?: boolean;
+}
+
 export const Transcript: FC<PropsWithChildren> = ({ children }) => (
   <div>
     <div className="transcript">{children}</div>
@@ -17,25 +21,40 @@ export const Avatar: FC<{ url: string; alt: string }> = ({ url, alt }) => (
 );
 
 export const Speech: FC<
-  PropsWithChildren<{ name: string; avatarUrl: string }>
-> = ({ name, avatarUrl, children }) => (
-  <div className="transcriptEntry">
-    <div className="transcriptSpeaker">
-      <Avatar url={avatarUrl} alt={name} />
-      <div className="transcriptSpeakerName">{name}</div>
-    </div>
-    <div className="transcriptText">{children}</div>
-  </div>
-);
+  PropsWithChildren<{ name: string; avatarUrl: string } & X>
+> = ({ name, avatarUrl, children, x }) => {
+  if (x && false) {
+    return (
+      <div className="transcriptEntryShort">
+        <div className="avatarShort">
+          <Avatar url={avatarUrl} alt={name} />
+        </div>
+        <div className="bodyShort">
+          <span className="transcriptSpeakerNameShort">{name}</span> {children}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={`transcriptEntry ${x ? "short" : ""}`}>
+        <div className="transcriptSpeaker">
+          <Avatar url={avatarUrl} alt={name} />
+          <div className="transcriptSpeakerName">{name}</div>
+        </div>
+        <div className="transcriptText">{children}</div>
+      </div>
+    );
+  }
+};
 
-export const B: FC<PropsWithChildren> = ({ children }) => (
-  <Speech name="Benjie" avatarUrl="/avatar/benjie.png">
+export const B: FC<PropsWithChildren<X>> = ({ children, x }) => (
+  <Speech x={x} name="Benjie" avatarUrl="/avatar/benjie.png">
     {children}
   </Speech>
 );
 
-export const J: FC<PropsWithChildren> = ({ children }) => (
-  <Speech name="Jem" avatarUrl="/avatar/jem.jpg">
+export const J: FC<PropsWithChildren<X>> = ({ children, x }) => (
+  <Speech x={x} name="Jem" avatarUrl="/avatar/jem.jpg">
     {children}
   </Speech>
 );
